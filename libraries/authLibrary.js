75b1e010ae63051bbdae6a89bd7973eaa3472e76
Pasquale.Iero <Pasquale.Iero@open.ac.uk>
2021-01-19 17:17:13 +0000

    Authorisation improved

--- libraries/authLibrary.js
+++ libraries/authLibrary.js
@@ -1,9 +1,9 @@
 // Authorization Rules Middleware
 // authLibrary.js
 
-const checkAuthorisation = (req, res, next) => {
+const checkAuthorisation = (req, res, next, permissions) => {
   const role = req.body.response_data.user.roles;
-  const passedRoles = ["recruiter"];
+  const passedRoles = permissions.roles ? permissions.roles : [] ;
 
   for ( var roleIndex=0; roleIndex< role.length; roleIndex++){
     if (passedRoles.includes(role[roleIndex])){
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 //Router
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 var authorisation = require('../libraries/authLibrary.js');
 
-router.post('/isAuthorised',  [authorisation.checkAuthorisation], (req, res, next) => {
+router.post('/isAuthorised',  [ (req, res, next) => {
+    authorisation.checkAuthorisation(req, res, next, {
+        roles : [ 'recruiter']})}], (req, res, next) => {
     res.send({});
 }, function(req, res, next) {
    res.status(200);
