ebf82b42a7af8142f1980e508c559132e2f9820e
Pasquale.Iero <Pasquale.Iero@open.ac.uk>
2021-01-20 15:11:21 +0000

    Using token data to authorise

--- libraries/authLibrary.js
+++ libraries/authLibrary.js
@@ -1,12 +1,29 @@
 // Authorization Rules Middleware
 // authLibrary.js
 
+var jwt = require('jsonwebtoken');
+
+function arrayToLowerCase (data) {
+  var result = [];
+  for ( var index = 0; index< data.length; index++){
+    result.push(data[index].toLowerCase());
+  }
+  return result;
+}
+
 const checkAuthorisation = (req, res, next, permissions) => {
+  var tokenFromJson = req.body.response_data.token.split(' ')[1];
+  var token = jwt.decode(tokenFromJson);
+
+  var tokenRoles = arrayToLowerCase(token.roles);
+
   const role = req.body.response_data.user.roles;
-  const passedRoles = permissions.roles ? permissions.roles : [] ;
+  const passedRoles = permissions.roles ? arrayToLowerCase(permissions.roles) : [] ;
+
+  const intersection = passedRoles.filter(value=>tokenRoles.includes(value));
 
   for ( var roleIndex=0; roleIndex< role.length; roleIndex++){
-    if (passedRoles.includes(role[roleIndex])){
+    if (intersection.includes(role[roleIndex])){
       next();
       return;
     }
