0fcc976943f6823e576ba5dd2efce13bbc674f0f
Pasquale.Iero <Pasquale.Iero@open.ac.uk>
2021-01-21 16:01:38 +0000

    Not using token plain data

--- libraries/authLibrary.js
+++ libraries/authLibrary.js
@@ -1,5 +1,6 @@
 // Authorization Rules Middleware
 // authLibrary.js
+require('../config.js');
 
 var jwt = require('jsonwebtoken');
 
@@ -15,18 +16,20 @@ const checkAuthorisation = (req, res, next, permissions) => {
   var tokenFromJson = req.body.response_data.token.split(' ')[1];
   var token = jwt.decode(tokenFromJson);
 
+  if (!jwt.verify(tokenFromJson, cfg.secret)){
+    return res.status(401).json({
+      status: 401,
+      message: 'UNAUTHORISED'
+    });
+  }
+
   var tokenRoles = arrayToLowerCase(token.roles);
-
-  const role = req.body.response_data.user.roles;
   const passedRoles = permissions.roles ? arrayToLowerCase(permissions.roles) : [] ;
-
   const intersection = passedRoles.filter(value=>tokenRoles.includes(value));
 
-  for ( var roleIndex=0; roleIndex< role.length; roleIndex++){
-    if (intersection.includes(role[roleIndex])){
-      next();
-      return;
-    }
+  if (intersection.length > 0){
+    next();
+    return;
   }
 
   return res.status(401).json({
